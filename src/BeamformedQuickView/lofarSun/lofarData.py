ff09234a6dee4cc1eb8b0acbbab1196a6a8052be
Peijin <pjer1316@icloud.com>
2019-09-24 21:07:42 +0800

    add fits

--- src/BeamformedQuickView/lofarSun/lofarData.py
+++ src/BeamformedQuickView/lofarSun/lofarData.py
@@ -8,6 +8,7 @@ import glob
 import os
 from astropy.io import fits as fits
 import numpy as np
+from skimage import measure
 from scipy.interpolate import griddata
 import matplotlib.pyplot as plt
 
@@ -77,10 +78,98 @@ class LofarDataBF:
         [X,Y,data_bf] = self.bf_image_by_idx(self,f_idx_select,t_idx_select,fov=fov,asecpix=asecpix,extrap=extrap,interpm=interpm)
         return [X,Y,data_bf]
 
-    def write_fits(self,dir,file_prefix,t_idx,f_idx):
+    def bf_peak_size(self,X,Y,data_bf,asecpix):
+        FWHM_thresh = np.max(data_bf)/2.0 #np.min(data_bf)+(np.max(data_bf)-np.min(data_bf))/2.0
+        img_bi = data_bf > FWHM_thresh
+        bw_lb = measure.label(img_bi)
+        rg_lb = measure.regionprops(bw_lb)
+        x_peak = X[np.where(data_bf == np.max(data_bf))]
+        y_peak = Y[np.where(data_bf == np.max(data_bf))]
+        rg_id = bw_lb[np.where(data_bf == np.max(data_bf))]
+        area_peak = rg_lb[int(rg_id)-1].area
+
+        return x_peak,y_peak,area_peak
+
+
+
+    def plot_bf_image_by_idx(self,f_idx,t_idx):
+        if True:
+            [X,Y,data_bf] = self.bf_image_by_idx(f_idx,t_idx,fov=3000,asecpix=20)
+            ax = plt.gca()
+            im = ax.imshow(data_bf, cmap='gist_heat',
+                      origin='lower',extent=[np.min(X),np.max(X),np.min(Y),np.max(Y)])
+            ax.set_xlabel('X (Arcsec)')
+            ax.set_ylabel('Y (Arcsec)')
+            ax.set_aspect('equal', 'box')
+            plt.colorbar(im)
+            FWHM_thresh = np.max(data_bf)/2.0 #np.min(data_bf)+(np.max(data_bf)-np.min(data_bf))/2.0
+            img_bi = data_bf > FWHM_thresh
+            ax.contour(X,Y,data_bf,levels=[FWHM_thresh,FWHM_thresh*2*0.9],colors=['deepskyblue','forestgreen'])
+            x_peak = X[np.where(data_bf == np.max(data_bf))]
+            y_peak = Y[np.where(data_bf == np.max(data_bf))]
+            
+            ax.plot(960*np.sin(np.arange(0,2*np.pi,0.001)),
+                        960*np.cos(np.arange(0,2*np.pi,0.001)),'w')
+            ax.plot(x_peak,y_peak,'k+')
+            plt.show()
+
+    def write_fits(self,fdir,fprefix,f_idx,t_idx):
         """
             write the data into fits files
         """
         if self.havedata:
-            cube_ds = self.data_cube[f_idx,t_idx,:]
+            cube_ds = self.data_cube[f_idx,:,:][:,t_idx,:]
+            hdu_lofar = fits.PrimaryHDU()
+            hdu_lofar.data = cube_ds.astype('float32')
+            print(self.data_cube.shape)
+            print(cube_ds.shape)
+            hdu_lofar.header['SIMPLE']    =                    True          
+            hdu_lofar.header['BITPIX']    =                    8 
+            hdu_lofar.header['NAXIS ']    =                    3          
+            hdu_lofar.header['NAXIS1']    =                 cube_ds.shape[0]      
+            hdu_lofar.header['NAXIS2']    =                 cube_ds.shape[1]      
+            hdu_lofar.header['NAXIS3']    =                 cube_ds.shape[2]
+            hdu_lofar.header['EXTEND']    =                    True               
+            hdu_lofar.header['DATE']      =  mdates.num2date(self.time_ds[t_idx[0]]).strftime("%Y-%m-%d")         
+            hdu_lofar.header['CONTENT']   =  mdates.num2date(self.time_ds[t_idx[0]]).strftime("%Y/%m/%d") + ' LOFAR Beamform observation '
+            hdu_lofar.header['ORIGIN']    = 'ASTRON Netherlands'
+            hdu_lofar.header['TELESCOP']  =  "LOFAR"
+            hdu_lofar.header['INSTRUME']  =  "LBA"          
+            hdu_lofar.header['OBJECT']    =  "Sun"         
+            hdu_lofar.header['DATE-OBS']  =  mdates.num2date(self.time_ds[t_idx[0]]).strftime("%Y/%m/%d")         
+            hdu_lofar.header['TIME-OBS']  =  mdates.num2date(self.time_ds[t_idx[0]]).strftime("%H:%M:%S.%f")       
+            hdu_lofar.header['DATE-END']  =  mdates.num2date(self.time_ds[t_idx[-1]]).strftime("%Y/%m/%d")         
+            hdu_lofar.header['TIME-END']  =  mdates.num2date(self.time_ds[t_idx[-1]]).strftime("%H:%M:%S.%f")          
+            hdu_lofar.header['BZERO']     =                   0. 
+            hdu_lofar.header['BSCALE']    =                   1. 
+            hdu_lofar.header['BUNIT']     = 'digits  '           
+            hdu_lofar.header['DATAMIN']   =                    np.min(cube_ds) 
+            hdu_lofar.header['DATAMAX']   =                    np.max(cube_ds) 
+            hdu_lofar.header['CRVAL1']    =                    self.freqs_ds[f_idx[0]]
+            hdu_lofar.header['CRPIX1']    =                    0
+            hdu_lofar.header['CTYPE1']    = 'FREQ'          
+            hdu_lofar.header['CDELT1']    =                    self.freqs_ds[f_idx[1]]-self.freqs_ds[f_idx[0]]
+            hdu_lofar.header['CRVAL2']    =                    self.time_ds[t_idx[0]]
+            hdu_lofar.header['CRPIX2']    =                    0 
+            hdu_lofar.header['CTYPE2']    = 'TIME'    
+            hdu_lofar.header['CDELT2']    =                    self.time_ds[t_idx[1]]-self.time_ds[t_idx[0]]  
+            hdu_lofar.header['CRVAL3']    =                    0 
+            hdu_lofar.header['CRPIX3']    =                    0 
+            hdu_lofar.header['CTYPE3']    = 'BEAM'    
+            hdu_lofar.header['CDELT3']    =                    1  
+            hdu_lofar.header['HISTORY']   = '        '    
 
+            col_f = fits.Column(name='FREQ',array=self.freqs_ds[f_idx],format="E")
+            col_t = fits.Column(name='TIME',array=self.time_ds[t_idx],format="E")
+            col_x = fits.Column(name='X',array=self.xb,format="E")
+            col_y = fits.Column(name='Y',array=self.yb,format="E")
+            hdu_f = fits.BinTableHDU.from_columns([col_f],name="FREQ")
+            hdu_t = fits.BinTableHDU.from_columns([col_t],name="TIME")
+            hdu_xy = fits.BinTableHDU.from_columns([col_x,col_y],name="BeamXY")
+
+            hdul = fits.HDUList([hdu_lofar,hdu_f,hdu_t,hdu_xy])
+            hdul.writeto(fprefix,overwrite=True)
+    
+    def write_fits_full(self,fdir,fprefix):
+        if self.havedata:
+            self.write_fits(fdir,fprefix,np.arange(len(self.freqs_ds)),np.arange(len(self.time_ds)))
--- src/BeamformedQuickView/test_fits_data.py
+++ src/BeamformedQuickView/test_fits_data.py
@@ -1,13 +1,9 @@
-from __future__ import absolute_import, division
-import glob
-import os
-import json
-from astropy.io import fits as fits
-import matplotlib.dates as mdates
-import datetime
-import numpy as np
-import matplotlib.pyplot as plt
 
-from lofarSun.lofarSun.lofardaData import lofarData
+# test the write fits module
+from lofarSun.lofarData import LofarDataBF
 
-
+lofar_bf = LofarDataBF()
+lofar_bf.load_sav("E:\\nextcloud\\meeting\\nacad\\2019_aut\\Lofar_spike\\data\\L599747_20170712_084137_short_data.sav")
+#[X,Y,data_bf] = lofar_bf.bf_image_by_idx(30,100)
+#lofar_bf.plot_bf_image_by_idx(30,100)
+#lofar_bf.write_fits_full('.','test.fits')
