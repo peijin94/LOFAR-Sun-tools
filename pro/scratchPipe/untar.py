59461f3fd3290c24bda64af93d6faed27e748115
Peijin Zhang <pjer1316@icloud.com>
2020-09-18 23:54:43 +0800

    new-pipe

---
+++ pro/scratchPipe/untar.py
@@ -0,0 +1,24 @@
+#M.C. Toribio
+#toribio@astron.nl
+#
+#Script to untar data retrieved from the LTA by using wget
+#It will DELETE the .tar file after extracting it.
+#
+# This scripts will rename those files as the string after the last '%'
+# If you want to change that behaviour, modify line
+# outname=filename.split("%")[-1]
+#
+# Version:
+# 2014/11/12: M.C. Toribio
+
+import os
+import glob
+
+for filename in glob.glob("*SAP*.tar*"):
+  outname=filename.split("%")[-1]
+  os.rename(filename, outname)
+  os.system('tar -xvf '+outname)
+  os.system('rm -r '+outname )
+
+  print outname+' untarred.'
+
