e9d531f61ad07467b96ad33aa3aeaa936f2c5062
Peijin Zhang (张沛锦) <pjer1316@mail.ustc.edu.cn>
2022-05-20 22:01:30 +0300

    gui resource fix

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ with open(path.join(this_directory,'README.md'), encoding='utf-8') as f:
 setup(
   name = 'lofarSun',         # How you named your package folder 
   packages = setuptools.find_packages(),#['lofarSun','lofarSun.IM','lofarSun.BF','lofarSun.BF.GUI'],   # Chose the same as "name"
-  version = '0.3.11',      # Start with a small number and increase it with every change you make
+  include_package_data=True,
+  version = '0.3.13',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'tools to process the lofar solar data',   # Give a short description about your library
   author = 'Peijin',                   # Type in your name
