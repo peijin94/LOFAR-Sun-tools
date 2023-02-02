bc9a68cd56804c4175b7f5f89c54d9bcbc2ee18e
Peijin Zhang <peijin@ustc.edu>
2023-02-02 12:35:54 +0200

    try speed up import

--- lofarSun/__init__.py
+++ lofarSun/__init__.py
@@ -1,6 +1,6 @@
 
-from lofarSun.IM import *
-from lofarSun.BF import *
-from lofarSun import cli
+#from lofarSun.IM import *
+#from lofarSun.BF import *
+#from lofarSun import cli
 
-__all__ = ['IMdata','BFcube','cli']
+#__all__ = ['IM','BF','cli']
